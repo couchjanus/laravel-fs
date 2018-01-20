01e8ccd90fc771c07a24e99e98ae21fc1476817f
couchjanus <couchjanus@gmail.com>
2018-01-20 12:30:19 +0200

    Update show.blade.php

--- resources/views/cms/roles/show.blade.php
+++ resources/views/cms/roles/show.blade.php
@@ -1,13 +1,21 @@
-@extends('cms.layouts.dashboard')
+@extends('layouts.cms.admin')
 
 @section('content')
-    <h3 class="page-title">Roles</h3>
-    
+    <!-- Main content -->
+    <section class="content">
+      <div class="row">
+        <div class="col-xs-12">
+          <div class="box">
+            <div class="box-header">
+              <h3 class="box-title">Dewtails Role</h3>
+            </div>
+            <!-- /.box-header -->
+            <div class="box-body table-responsive no-padding">
+
+
+
+
     <div class="panel panel-default">
-        <div class="panel-heading">
-            View
-        </div>
-        
         <div class="panel-body">
             <div class="row">
                 <div class="col-md-6">
@@ -22,5 +30,11 @@
 
             <a href="{{ route('roles.index') }}" class="btn btn-default">Back to list</a>
         </div>
+      </div>
+      <!-- /.box-body -->
     </div>
-@stop
+    <!-- /.box -->
+  </div>
+  </div>
+  </section>
+  @stop
